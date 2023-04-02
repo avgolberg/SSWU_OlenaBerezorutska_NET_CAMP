ce6411c387e4d0c77e895eb34395a05113fb8e94
Alyona Berezorutskaya <av.golberg@gmail.com>
2023-04-02 19:11:29 +0300

    Add Exercise 2 to Home Task 3

---
+++ Home_task_3/Exercise_2/WorkWithText/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WorkWithText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string text = @"Parrot is a kind of bird. My Parrot preffer eatting seeds.";
+                WorkWithText workWithText = new WorkWithText(text);
+                Console.WriteLine($"Text is \"{workWithText}\"");
+
+                Console.WriteLine($"Second index of substring \"Parrot\" is {workWithText.SecondIndexOfSubstring("Parrot")}");
+                Console.WriteLine($"Count of words that start with uppercase letter is {workWithText.WordsStartWithUpper()}");
+                Console.WriteLine($"Replaced words with duplicate chars by word \"Yes\": {workWithText.ReplaceWordsWithDuplicateChars("Yes")}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Sorry, an unexpected error ocured");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Home_task_3/Exercise_2/WorkWithText/WorkWithText.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+
+namespace WorkWithText
+{
+    class WorkWithText
+    {
+        private string _text;
+
+        public WorkWithText(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                throw new ArgumentException($"{nameof(text)} can not be null or white space", nameof(text));
+
+            _text = RemovePunctuations(text);
+        }
+        public string RemovePunctuations(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in input)
+            {
+                if (!char.IsPunctuation(c))
+                    sb.Append(c);
+            }
+            return sb.ToString();
+        }
+
+        public int? SecondIndexOfSubstring(string substring)
+        {
+            if (string.IsNullOrWhiteSpace(substring) || !_text.Contains(substring))
+                return null;
+
+            int index = _text.IndexOf(substring);
+            index = _text.IndexOf(substring, index + substring.Length);
+            return (index != -1) ? index : null;
+        }
+
+        public int WordsStartWithUpper()
+        {
+            var words = _text.Split();
+            int count = 0;
+            foreach (string word in words)
+            {
+                if (char.IsUpper(word, 0)) count++;
+            }
+            return count;
+        }
+
+        public bool ContainsDuplicateChars(string str)
+        {
+            bool containsDuplicates = false;
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                if (str[i] == str[i + 1])
+                {
+                    containsDuplicates = true;
+                    break;
+                }
+            }
+            return containsDuplicates;
+        }
+
+        public string ReplaceWordsWithDuplicateChars(string replace)
+        {
+            var words = _text.Split();
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (ContainsDuplicateChars(words[i]))
+                    words[i] = replace;
+            }
+            return string.Join(" ", words);
+        }
+
+        public override string ToString()
+        {
+            return _text;
+        }
+    }
+}
