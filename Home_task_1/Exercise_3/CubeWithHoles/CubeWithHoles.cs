e4b2267f419d18fb7cb422e87dc6c0ee2473cc93
Alyona Berezorutskaya <av.golberg@gmail.com>
2023-04-03 20:54:11 +0300

    Update Exercise 3 in Home Task 1

--- Home_task_1/Exercise_3/CubeWithHoles/CubeWithHoles.cs
+++ Home_task_1/Exercise_3/CubeWithHoles/CubeWithHoles.cs
@@ -50,11 +50,13 @@ namespace CubeWithHoles
     {
         private uint _size;
         private int[,,] _cube;
+        private uint _middle;
 
         private List<Hole> _holes;
-        public CubeWithHoles(uint size = 3)
+        public CubeWithHoles(uint size = 5)
         {
             _size = size;
+            _middle = _size / 2;
             _cube = new int[_size, _size, _size];
             _holes = new List<Hole>();
 
@@ -83,22 +85,13 @@ namespace CubeWithHoles
                             countDeepLine++;
 
                         if (countHorizontalLine == _size)
-                        {
-                            var hole = new Hole(HoleType.Horizontal, new uint[] { i, j, k + 1 - _size }, new uint[] { i, j, k }); 
-                            _holes.Add(hole);
-                        }
+                            _holes.Add(new Hole(HoleType.Horizontal, new uint[] { i, j, k + 1 - _size }, new uint[] { i, j, k }));
 
                         if (countVerticalLine == _size)
-                        {
-                            var hole = new Hole(HoleType.Vertical, new uint[] { i,  k + 1 - _size, j }, new uint[] { i, k, j });
-                            _holes.Add(hole);
-                        }
+                            _holes.Add(new Hole(HoleType.Vertical, new uint[] { i, k + 1 - _size, j }, new uint[] { i, k, j }));
 
                         if (countDeepLine == _size)
-                        {
-                            var hole = new Hole(HoleType.Deep, new uint[] {k + 1 - _size, j, i }, new uint[] { k, j, i });
-                            _holes.Add(hole);
-                        }
+                            _holes.Add(new Hole(HoleType.Deep, new uint[] { k + 1 - _size, j, i }, new uint[] { k, j, i }));
                     }
                     countHorizontalLine = 0;
                     countVerticalLine = 0;
@@ -106,7 +99,6 @@ namespace CubeWithHoles
                 }
             }
 
-            //треба виключати складові отворів для розрядностей більше 3
             if (_size % 2 == 0)
                 FindDiagonalEven();
             else
@@ -115,26 +107,36 @@ namespace CubeWithHoles
 
         private void FindDiagonalOdd()
         {
-            uint middle = _size / 2;
             for (uint i = 0; i < _size; i++)
             {
                 for (uint j = 0; j < _size; j++)
                 {
                     for (uint k = 0; k < _size; k++)
                     {
+                        if (i == _middle) return;
+
                         if (_cube[k, j, i] == 0
-                          && _cube[middle, j, middle] == 0
-                          && k != middle
-                          && ((_cube[k, j, k] == 0 && _cube[_size - 1 - k, j, _size - 1 - k] == 0)
-                          || (_cube[_size - 1 - k, j, k] == 0 && _cube[k, j, _size - 1 - k] == 0)))
+                          && _cube[_middle, j, _middle] == 0
+                          && k != _middle
+                          && i != _middle - 1)
                         {
-                            if (_cube[k, j, k] == 0 && _holes.Find(h => Enumerable.SequenceEqual(h.StartIndex, new uint[] { _size - 1 - k, j, _size - 1 - k })) == null)
+                            if (_cube[i, j, k] == 0 && _cube[_size - 1 - i, j, _size - 1 - k] == 0)
                             {
-                                _holes.Add(new Hole(HoleType.Diagonal, new uint[] { k, j, k }, new uint[] { _size - 1 - k, j, _size - 1 - k }));
+                                if (!DiagonalHoleExists(new uint[] { i, j, k }, new uint[] { _size - 1 - i, j, _size - 1 - k })
+                                    && !DiagonalHoleExists(new uint[] { _size - 1 - i, j, _size - 1 - k }, new uint[] { i, j, k }) 
+                                    && NextHolesExists(new uint[] { i, j, k }))
+                                {
+                                    _holes.Add(new Hole(HoleType.Diagonal, new uint[] { i, j, k }, new uint[] { _size - 1 - i, j, _size - 1 - k }));
+                                }
                             }
-                            if (_cube[_size - 1 - k, j, k] == 0 && _holes.Find(h => Enumerable.SequenceEqual(h.StartIndex, new uint[] { _size - 1 - k, j, k })) == null)
+                            if (_cube[i, j, _size - 1 - k] == 0 && _cube[_size - 1 - i, j, k] == 0)
                             {
-                                _holes.Add(new Hole(HoleType.Diagonal, new uint[] { k, j, _size - 1 - k }, new uint[] { _size - 1 - k, j, k }));
+                                if (!DiagonalHoleExists(new uint[] { i, j, _size - 1 - k }, new uint[] { _size - 1 - i, j, k })
+                                    && !DiagonalHoleExists(new uint[] { _size - 1 - i, j, k }, new uint[] { i, j, _size - 1 - k }) 
+                                    && NextHolesExists(new uint[] { i, j, _size - 1 - k }))
+                                {
+                                    _holes.Add(new Hole(HoleType.Diagonal, new uint[] { i, j, _size - 1 - k }, new uint[] { _size - 1 - i, j, k }));
+                                }
                             }
                         }
                     }
@@ -150,24 +152,59 @@ namespace CubeWithHoles
                 {
                     for (uint k = 0; k < _size; k++)
                     {
+                        if (i == _middle - 1) return;
+
                         if (_cube[k, j, i] == 0
-                          && ((_cube[k, j, k] == 0 && _cube[_size - 1 - k, j, _size - 1 - k] == 0)
-                          || (_cube[_size - 1 - k, j, k] == 0 && _cube[k, j, _size - 1 - k] == 0)))
+                            && _cube[_middle - 1, j, _middle - 1] == 0
+                            && _cube[_middle, j, _middle] == 0
+                            && k != _middle)
                         {
-                            if (_cube[k, j, k] == 0 && _holes.Find(h => Enumerable.SequenceEqual(h.StartIndex, new uint[] { _size - 1 - k, j, _size - 1 - k })) == null)
+                            if (_cube[i, j, k] == 0 && _cube[_size - 1 - i, j, _size - 1 - k] == 0)
                             {
-                                _holes.Add(new Hole(HoleType.Diagonal, new uint[] { k, j, k }, new uint[] { _size - 1 - k, j, _size - 1 - k }));
+                                if (!DiagonalHoleExists(new uint[] { i, j, k }, new uint[] { _size - 1 - i, j, _size - 1 - k })
+                                    && !DiagonalHoleExists(new uint[] { _size - 1 - i, j, _size - 1 - k }, new uint[] { i, j, k }) 
+                                    && NextHolesExists(new uint[] { i, j, k }))
+                                {
+                                    _holes.Add(new Hole(HoleType.Diagonal, new uint[] { i, j, k }, new uint[] { _size - 1 - i, j, _size - 1 - k }));
+                                }
+
                             }
-                            if (_cube[_size - 1 - k, j, k] == 0 && _holes.Find(h => Enumerable.SequenceEqual(h.StartIndex, new uint[] { _size - 1 - k, j, k })) == null)
+                            if (_cube[i, j, _size - 1 - k] == 0 && _cube[_size - 1 - i, j, k] == 0)
                             {
-                                _holes.Add(new Hole(HoleType.Diagonal, new uint[] { k, j, _size - 1 - k }, new uint[] { _size - 1 - k, j, k }));
+                                if (!DiagonalHoleExists(new uint[] { i, j, _size - 1 - k }, new uint[] { _size - 1 - i, j, k })
+                                    && !DiagonalHoleExists(new uint[] { _size - 1 - i, j, k }, new uint[] { i, j, _size - 1 - k }) 
+                                    && NextHolesExists(new uint[] { i, j, _size - 1 - k }))
+                                {
+                                    _holes.Add(new Hole(HoleType.Diagonal, new uint[] { i, j, _size - 1 - k }, new uint[] { _size - 1 - i, j, k }));
+                                }
+
                             }
                         }
                     }
                 }
             }
         }
-
+        private bool DiagonalHoleExists(uint[] StartIndex, uint[] EndIndex)
+        {
+            return _holes.Exists(h => h.Type == HoleType.Diagonal && Enumerable.SequenceEqual(h.StartIndex, StartIndex) && Enumerable.SequenceEqual(h.EndIndex, EndIndex));
+        }
+        private bool NextHolesExists(uint[] StartIndex)
+        {
+            for (uint i = StartIndex[0] + 1; i < _size; i++)
+            {
+                if (StartIndex[0] == StartIndex[2])
+                {
+                    if (!(_cube[i, StartIndex[1], i] == 0 && _cube[_size - 1 - i, StartIndex[1], _size - 1 - i] == 0))
+                        return false;
+                }
+                else
+                {
+                    if (!(_cube[i, StartIndex[1], _size - 1 - i] == 0 && _cube[_size - 1 - i, StartIndex[1], i] == 0))
+                        return false;
+                }
+            }
+            return true;
+        }
         private void Fill()
         {
             var rand = new Random();
